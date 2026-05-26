eb6fde999ff0b5a79bf460c88b3d3378edbbbf47
Lee ByungSeo <bslee@yahoo.com>
2026-05-26 20:56:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-# from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 from datetime import datetime
 import time
 import streamlit.components.v1 as components
 import cv2
-import mediapipe as mp
 import numpy as np
+# from streamlit_gsheets import GSheetsConnection  <-- 일단 주석 처리
+# import mediapipe as mp                           <-- 일단 주석 처리
 
 # --- 1. 세션 상태 초기화 ---
 if "logged_in" not in st.session_state:
